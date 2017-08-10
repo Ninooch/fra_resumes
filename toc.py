5fc51c68d5741cd1a5ea057ece76d6a38c8f8d11
Niels Lachat <nielsnfsmw@gmail.com>
2017-08-10 15:37:48 +0200

    update

--- toc.py
+++ toc.py
@@ -7,6 +7,6 @@ els=int(sys.argv[2])
 oStr=""
 
 for i in range(1,els+1):
-    oStr+="* ["+bn+" "+str(i)+"]("+bnL+"-"+str(i)+")\n"
+    oStr+="* ["+bn+" "+str(i)+"](#"+bnL+"-"+str(i)+")\n"
 
 print(oStr)
